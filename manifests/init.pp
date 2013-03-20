1c1d9486d3038ded77f98541ed5c7bb1b888bfc3
Jean-Luc Geering <jeanluc.geering@ufirstgroup.com>
2013-03-20 10:39:25 +0100

    updated SourceTree to 1.5.8

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class sourcetree {
   package { 'SourceTree':
     provider => 'appdmg',
-    source   => 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.5.7.1.dmg'
+    source   => 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.5.8.dmg'
   }
 }
