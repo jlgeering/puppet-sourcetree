b7d8c78f1bf9c2c3195101ca6328fa1ea8fee913
Nahuel Scotti <nahuel.scotti@gmail.com>
2013-05-09 16:42:24 +0200

    updated to SourceTree 1.6.0.1

--- spec/classes/sourcetree_spec.rb
+++ spec/classes/sourcetree_spec.rb
@@ -4,7 +4,7 @@ describe 'sourcetree' do
   it do
     should contain_package('SourceTree').with({
       :provider => 'appdmg',
-      :source   => 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.6.0.dmg',
+      :source   => 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.6.0.1.dmg',
     })
   end
 end
